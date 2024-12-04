62e43a2bef744d0677b0e06e3c5c09ed19a93aa2
elnarafandi <elnaraafandiyeva5@gmail.com>
2024-12-04 19:58:05 +0400

    commit3

--- TicketBookingProject/ActionOnTicketItems.Designer.cs
+++ TicketBookingProject/ActionOnTicketItems.Designer.cs
@@ -218,7 +218,7 @@
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Yellow;
-            ClientSize = new Size(945, 718);
+            ClientSize = new Size(945, 670);
             Controls.Add(label7);
             Controls.Add(dataGridView1);
             Controls.Add(btnDelete);
